7e902a88217e66b099f97b578e7f5244fcda9ba1
szTheory <szTheory@users.noreply.github.com>
2026-05-01 12:43:11 -0400

    feat(28-01): implement ImageParser for intrinsic dimensions
    - Extract width, height and mime from PNG
    - Extract width, height and mime from JPEG
    - Handle unsupported formats

---
+++ lib/rendro/image_parser.ex
@@ -0,0 +1,51 @@
+defmodule Rendro.ImageParser do
+  @moduledoc """
+  Pure functional parser for extracting intrinsic bounds and mime types from image binaries.
+  """
+
+  @png_signature <<137, 80, 78, 71, 13, 10, 26, 10>>
+  @jpeg_signature <<0xFF, 0xD8>>
+
+  @sof_markers [
+    0xC0, 0xC1, 0xC2, 0xC3, 0xC5, 0xC6, 0xC7, 0xC9, 0xCA, 0xCB, 0xCD, 0xCE, 0xCF
+  ]
+
+  @spec parse(binary()) ::
+          {:ok, %{width: pos_integer(), height: pos_integer(), mime: String.t()}}
+          | {:error, term()}
+  def parse(<<@png_signature, _length::32, "IHDR", width::32, height::32, _rest::binary>>) do
+    {:ok, %{width: width, height: height, mime: "image/png"}}
+  end
+
+  def parse(<<@jpeg_signature, rest::binary>>) do
+    parse_jpeg_markers(rest)
+  end
+
+  def parse(_bytes) do
+    {:error, :unsupported_image_format}
+  end
+
+  defp parse_jpeg_markers(<<0xFF, 0xFF, rest::binary>>) do
+    parse_jpeg_markers(<<0xFF, rest::binary>>)
+  end
+
+  defp parse_jpeg_markers(<<0xFF, marker, length::16, rest::binary>>)
+       when marker not in [0x00, 0xFF, 0xD8, 0xD9] do
+    if marker in @sof_markers do
+      <<_precision::8, height::16, width::16, _::binary>> = rest
+      {:ok, %{width: width, height: height, mime: "image/jpeg"}}
+    else
+      chunk_data_len = length - 2
+      case rest do
+        <<_skip::binary-size(chunk_data_len), next::binary>> ->
+          parse_jpeg_markers(next)
+        _ ->
+          {:error, :unsupported_image_format}
+      end
+    end
+  end
+
+  defp parse_jpeg_markers(_) do
+    {:error, :unsupported_image_format}
+  end
+end
---
+++ test/rendro/image_parser_test.exs
@@ -0,0 +1,25 @@
+defmodule Rendro.ImageParserTest do
+  use ExUnit.Case, async: true
+  alias Rendro.ImageParser
+
+  # 2x2 PNG
+  @png_bytes Base.decode64!("iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAFElEQVQIW2NkYGD4z8DAwMgAI0AMADjKAu09+3WTAAAAAElFTkSuQmCC")
+  
+  # 1x1 JPEG
+  @jpeg_bytes Base.decode64!("/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAP//////////////////////////////////////////////////////////////////////////////////////wgALCAABAAEBAREA/8QAFBABAAAAAAAAAAAAAAAAAAAAAP/aAAgBAQABPxA=")
+
+  describe "parse/1" do
+    test "extracts width, height, and mime from valid PNG bytes" do
+      assert {:ok, %{width: 2, height: 2, mime: "image/png"}} = ImageParser.parse(@png_bytes)
+    end
+
+    test "extracts width, height, and mime from valid JPEG bytes" do
+      assert {:ok, %{width: 1, height: 1, mime: "image/jpeg"}} = ImageParser.parse(@jpeg_bytes)
+    end
+
+    test "returns error for invalid/unsupported bytes" do
+      assert {:error, :unsupported_image_format} = ImageParser.parse(<<"invalid">>)
+      assert {:error, :unsupported_image_format} = ImageParser.parse(<<0xFF, 0xD8, 0xFF, 0x00>>)
+    end
+  end
+end
