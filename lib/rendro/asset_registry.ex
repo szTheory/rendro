cdbba80fa95adb28c81e8dcc3407694e03c3b5b5
szTheory <szTheory@users.noreply.github.com>
2026-05-01 12:45:22 -0400

    feat(28-01): implement AssetRegistry and integrate with Document

--- lib/rendro/asset_registry.ex
+++ lib/rendro/asset_registry.ex
@@ -9,13 +9,50 @@ defmodule Rendro.AssetRegistry do
 
   defstruct assets: %{}
 
+  @type t :: %__MODULE__{
+          assets: %{
+            optional(atom()) => %{
+              binary: binary(),
+              width: pos_integer(),
+              height: pos_integer(),
+              mime: String.t()
+            }
+          }
+        }
+
+  @spec new() :: t()
   def new, do: %__MODULE__{}
 
-  def register_image(_registry, _logical_name, _source) do
-    # Not implemented
+  @doc """
+  Registers an image into the registry.
+  Source can be `{:binary, binary()}` or `{:path, Path.t()}`.
+  """
+  @spec register_image(t(), atom(), {:binary, binary()} | {:path, Path.t()}) :: t()
+  def register_image(%__MODULE__{} = registry, logical_name, source) when is_atom(logical_name) do
+    binary =
+      case source do
+        {:binary, bytes} when is_binary(bytes) -> bytes
+        {:path, path} when is_binary(path) -> File.read!(path)
+      end
+
+    case Rendro.ImageParser.parse(binary) do
+      {:ok, %{width: width, height: height, mime: mime}} ->
+        asset = %{binary: binary, width: width, height: height, mime: mime}
+        %__MODULE__{registry | assets: Map.put(registry.assets, logical_name, asset)}
+
+      {:error, reason} ->
+        raise InvalidAssetError,
+          message: "Failed to parse image for asset #{inspect(logical_name)}: #{inspect(reason)}",
+          logical_name: logical_name,
+          reason: reason
+    end
   end
-  
-  def fetch(_registry, _logical_name) do
-    :error
+
+  @doc """
+  Fetches a registered asset.
+  """
+  @spec fetch(t(), atom()) :: {:ok, map()} | :error
+  def fetch(%__MODULE__{} = registry, logical_name) when is_atom(logical_name) do
+    Map.fetch(registry.assets, logical_name)
   end
 end
